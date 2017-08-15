a463908d974b0348fe0bee3b48e28a00ac68d37d
yakeing <yakeing@gmail.com>
2017-08-15 19:00:24 +0800

    test jwsign

--- tests/php_jwsignTest/php_jwsignTest.php
+++ tests/php_jwsignTest/php_jwsignTest.php
@@ -9,21 +9,34 @@ class php_jwsignTest extends TestCase{
 
   public function testSign(){
     $jws = new jwsign();
-    $Message = 'Helloworld';
+    $Message = base64_encode('
+        {
+            "method":"pay",
+            "charset":"utf-8",
+            "version":"1.0",
+            "token":"074zyzi52z4JYBXSBspgyDo"
+        }
+    ');
     $privateFile = dirname(__FILE__).'/private.key';
-    //$accesskey = file_get_contents($privateFile);
-    $this->assertTrue(is_file($privateFile));
-    // $jws = new jwsign();
-    // $jws->SetPrivate($accesskey);
-    // $Pubkey = $jws->GetPubkey();
-    // $this->assertTrue(is_array($Pubkey));
-    // $this->assertEquals(2048, $Pubkey['bits']);
-    // $this->assertEquals('Helloworld', $Pubkey['kid']);
-    // $JsonStr = $jws->SignMessage($Message);
-    // $this->assertTrue(!is_bool($JsonStr));
-    // return array($Pubkey['pub'], $JsonStr);
-    $this->assertEquals(200, 100);
+    //$this->assertTrue(is_file($privateFile));
+    $accesskey = file_get_contents($privateFile);
+    $jws = new jwsign();
+    $jws->SetPrivate($accesskey);
+    $Pubkey = $jws->GetPubkey();
+    $JsonStr = $jws->SignMessage($Message);
+    $this->assertEquals(2048, $Pubkey['bits']);
+    $this->assertTrue(is_string($JsonStr));
+    return array($jws, $Pubkey['pub'], $JsonStr);
   }
 
-  public function testVerify(){}
+  public function testVerify($args){
+    list($jws, $pub, $JsonStr) = $args;
+    $arrayStr = json_decode($JsonStr, true);
+    $this->assertTrue(isset($arrayStr['sign']));
+    $sign = $arrayStr['sign'];
+    unset($arrayStr['sign']);
+    $value = json_encode($arrayStr);
+    $Verify = $jws->PubkeyVerify($value, $sign, $pub);
+    $this->assertTrue($Verify);
+  }
 }
