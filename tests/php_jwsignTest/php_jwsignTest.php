42936013375284741604312fd62680820d2d2046
yakeing <yakeing@gmail.com>
2017-08-15 18:32:01 +0800

    u

--- tests/php_jwsignTest/php_jwsignTest.php
+++ tests/php_jwsignTest/php_jwsignTest.php
@@ -7,10 +7,24 @@ use PHPUnit\Framework\TestCase;
 
 class php_jwsignTest extends TestCase{
 
-  public function testGet(){
-    $curl = new jwsign();
+  public function testSign(){
+    $jws = new jwsign();
+    $Message = 'Helloworld';
+    //$privateFile = 'private.key';
+    $privateFile = dirname(__FILE__);
+    //$accesskey = file_get_contents($privateFile);
+    $this->assertEquals('uuu', $privateFile);
+    // $jws = new jwsign();
+    // $jws->SetPrivate($accesskey);
+    // $Pubkey = $jws->GetPubkey();
+    // $this->assertTrue(is_array($Pubkey));
+    // $this->assertEquals(2048, $Pubkey['bits']);
+    // $this->assertEquals('Helloworld', $Pubkey['kid']);
+    // $JsonStr = $jws->SignMessage($Message);
+    // $this->assertTrue(!is_bool($JsonStr));
+    // return array($Pubkey['pub'], $JsonStr);
     $this->assertEquals(200, 100);
   }
 
-  public function testPost(){}
+  public function testVerify(){}
 }
